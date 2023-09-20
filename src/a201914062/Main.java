7627b1095ca1e57f40f53c9aeb31ab46e3967ccb
Lee yeon Jeen <jeendev2019@gmail.com>
2023-09-20 10:47:16 +0900

    five

--- src/a201914062/Main.java
+++ src/a201914062/Main.java
@@ -3,9 +3,10 @@ package a201914062;
 public class Main {
 	public static void main(String[] args) { 
 		
+		Five.print(); 
 		Two.print(); 
 		System.out.println("one"); 
-		Three.print(); Four.print();
+		Three.print();
 	}
 
 }
